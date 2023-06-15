a857394b4776d0b992fff9057d6dabe34567ba1f
Ndjido Ardo Fall <fall.ndjidoardo@yahoo.fr>
2023-06-15 21:08:13 +0000

    Writing host_usage.sh

---
+++ linux_sql/scripts/host_usage.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+if [ "$#" -ne 5 ]; then
+  echo "Illegal number of parameters"
+  exit 1
+fi
+
+vmstat_mb=$(vmstat --unit M)
+hostname=$(hostname -f)
+
+memory_free=$(echo "$vmstat_mb" | awk '{print $4}' | tail -n1 | xargs)
+cpu_idle=$(echo "$vmstat_mb" | awk '{print $15}' | tail -n1 | xargs)
+cpu_kernel=$(echo "$vmstat_mb" | awk '{print $14}' | tail -n1 | xargs)
+disk_io=$(vmstat -d | awk '{print $10}' | tail -n1 | xargs)
+disk_available=$(df -BM / ...)
+
+timestamp=$(vmstat -t | awk '{print $18, $19}' | tail -n1 | xargs)
