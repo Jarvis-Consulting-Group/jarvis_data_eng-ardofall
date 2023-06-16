df0ddaf7f4e091d74751b35aa79a5b172fe2dd7d
Ndjido Ardo Fall <fall.ndjidoardo@yahoo.fr>
2023-06-16 22:09:49 +0000

    Updated scripts

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -14,15 +14,15 @@ lscpu_out=`lscpu`
 hostname=$(hostname -f)
 
 cpu_number= $(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
-cpu_architecture= $(echo "lscpu_out"| egrep "^Architecture:" | awk '{print $2}' | xargs)
-cpu_model=  $(echo "lscpu_out" | egrep "Model name:" | awk '{print $3,$4,$5,$6,$7}' | xargs)
-cpu_mhz= $(echo "lscpu_out" | egrep "CPU MHz:" | awk '{print $3}' | xargs)
-l2_cache= $(echo "lscpu_out" | egrep "^L2" | awk '{print $3}' | xargs)
+cpu_architecture= $(echo "$lscpu_out"| egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=  $(echo "$lscpu_out" | egrep "Model name:" | awk '{print $3,$4,$5,$6,$7}' | xargs)
+cpu_mhz= $(echo "$lscpu_out" | egrep "CPU MHz:" | awk '{print $3}' | xargs)
+l2_cache= $(echo "$lscpu_out" | egrep "^L2" | awk '{print $3}' | xargs)
 total_mem= $(vmstat --unit M | tail -1 | awk '{print $4}')
 timestamp= $(date -u '+%Y-%m-%d %H:%M:%S')
 
 id= 1
-insert_stmt="INSERT INTO host_usage(id, hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, timestamp, total_mem) VALUES('$id', '$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache', '$timestamp', '$total_mem')"
+insert_stmt="INSERT INTO host_usage(id, hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, timestamp, total_mem) VALUES("$id", "$hostname", "$cpu_number", "$cpu_architecture", "$cpu_model", "$cpu_mhz", "$l2_cache", "$timestamp", "$total_mem")"
 
 export PGPASSWORD=$psql_password
 
